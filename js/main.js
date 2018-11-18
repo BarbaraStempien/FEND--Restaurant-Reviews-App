cd7bec3b661abb8e5e0e3122ca9bd918d468ac0e
barbarastempien <barbara.stempien@gmail.com>
2018-11-18 01:05:36 +0100

    feat: Add  MapBox API

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYmFyYmFyYXN0cCIsImEiOiJjam9tM2EzdnEwZmZ3M3FvMmVlM3hjNWd6In0.-1biC9GNHY7GI-I3Zj-CXQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
